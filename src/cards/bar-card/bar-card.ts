57102e12b7412782007cd1988b90e552e8eb4b91
Justin Crause <juzz@crause.com>
2023-10-04 11:20:13 +0200

    Rebuilding the BarCard Standalone

--- src/cards/bar-card/bar-card.ts
+++ src/cards/bar-card/bar-card.ts
@@ -12,13 +12,13 @@ import {
     LovelaceCard,
     LovelaceCardEditor,
 } from "../../ha";
-import "../../shared/badge-icon";
-import "../../shared/button";
-import "../../shared/card";
-import "../../shared/shape-avatar";
-import "../../shared/shape-icon";
-import "../../shared/state-info";
-import "../../shared/state-item";
+// import "../../shared/badge-icon";
+// import "../../shared/button";
+// import "../../shared/card";
+// import "../../shared/shape-avatar";
+// import "../../shared/shape-icon";
+// import "../../shared/state-info";
+// import "../../shared/state-item";
 import "../../shared/slider-ex";
 import { MushroomBaseCard } from "../../utils/base-card";
 import { cardStyle } from "../../utils/card-styles";
